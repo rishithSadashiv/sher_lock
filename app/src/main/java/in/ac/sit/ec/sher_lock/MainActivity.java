f0f0232bcf77c6016cbe598caffb62a89d98142f
rishithSadashiv <rishith.1si16ec078@gmail.com>
2019-10-15 14:11:01 +0530

    apk added

--- app/src/main/java/in/ac/sit/ec/sher_lock/MainActivity.java
+++ app/src/main/java/in/ac/sit/ec/sher_lock/MainActivity.java
@@ -1,7 +1,6 @@
 package in.ac.sit.ec.sher_lock;
 
-import android.content.Context;
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,10 +18,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
-import java.io.File;
-import java.util.EventListener;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
     static String link2 = "https://firebasestorage.googleapis.com/v0/b/badhackathon.appspot.com/o/unknown.png?alt=media";
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,29 +53,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 allow.child("value").setValue(true);
-//                someName.addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                        HashMap<String,String> ob2;
-//                        ob2 = (HashMap<String, String>) dataSnapshot.getValue();
-//                        String value = ob2.get("value");
-//                        try {
-//                            if (value == null || value.isEmpty()) {
-//                                unknownName.setVisibility(View.VISIBLE);
-//                                submitName.setVisibility(View.VISIBLE);
-//                            }
-//                        } catch (NullPointerException e) {
-//                            unknownName.setVisibility(View.VISIBLE);
-//                            submitName.setVisibility(View.VISIBLE);
-//                        }
-//                    }
-//
-//
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                    }
-//                });
+
 
             }
         });
@@ -93,23 +64,6 @@ public class MainActivity extends AppCompatActivity {
                 allow.child("value").setValue(false);
             }
         });
-
-//        submitName.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String text = unknownName.getText().toString();
-//
-//                if (text.isEmpty())
-//                    Toast.makeText(MainActivity.this, "Please enter valid name.", Toast.LENGTH_SHORT).show();
-//                else {
-//                    someName.child("value").setValue(text);
-//                    unknownName.setVisibility(View.INVISIBLE);
-//                    submitName.setVisibility(View.INVISIBLE);
-//                }
-//            }
-//
-//
-//        });
 
 
         refresh.setOnClickListener(new View.OnClickListener() {
@@ -133,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-//                String link1 = dataSnapshot.getValue(String.class);
                         if (known)
                             Picasso.get().load(link).into(imageView);
                         else
@@ -153,14 +106,6 @@ public class MainActivity extends AppCompatActivity {
                         String val2 = dataSnapshot.getValue(String.class);
 
                         System.out.println(val2);
-//                try {
-////                    if (val2 == null || val2.isEmpty())
-////                        imName.setText("Unknown Person");
-////                    else
-////                        imName.setText(val2);
-//                } catch (Exception e) {
-//                    imName.setText("Unknown Person");
-//                }
                         imName.setText(val2);
                     }
 
@@ -171,66 +116,6 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
-
-
-
-
-
-
-
-//                finish();
-//                startActivity(getIntent());
-
-//                Context context = getApplicationContext();
-//                context.getCacheDir().deleteOnExit();
-//
-//
-//                finish();
-//                overridePendingTransition(0, 0);
-//                startActivity(getIntent());
-//                overridePendingTransition(0, 0);
-//
-
-
-
-//                someName.addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                        HashMap<String,String> ob1;
-//                        ob1 = (HashMap<String, String>) dataSnapshot.getValue();
-//                        String value = ob1.get("value");
-//                        try {
-//                            if (value.isEmpty())
-//                                imName.setText("Unknown Person");
-//                            else
-//                                imName.setText(value);
-//                        } catch (NullPointerException e) {
-//                            imName.setText("Unknown Person");
-//                        }
-//
-//
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                    }
-//                });
-//
-//                image.addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                        Picasso.get().load(link).into(imageView);
-//                    }
-//
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                    }
-//                });
-//
-//
-//
             }
 
         });
@@ -279,14 +164,7 @@ public class MainActivity extends AppCompatActivity {
                 String val2 = dataSnapshot.getValue(String.class);
 
                 System.out.println(val2);
-//                try {
-////                    if (val2 == null || val2.isEmpty())
-////                        imName.setText("Unknown Person");
-////                    else
-////                        imName.setText(val2);
-//                } catch (Exception e) {
-//                    imName.setText("Unknown Person");
-//                }
+
                 imName.setText(val2);
             }
 
@@ -297,8 +175,4 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-
-
-
 }
